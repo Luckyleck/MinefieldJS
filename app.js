a522cf59e53a8b57bce76b94508baa3efb4444bc
Alex <luckyleck29@gmail.com>
2023-11-16 19:29:59 -0500

    Adding counter functions and button logic

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-import Minefield from './src/minefield.js'
+import Minefield from './src/minefield.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const minefield = new Minefield(10);
+    let minefield = new Minefield(10);
     minefield.initializeMines();
     minefield.drawBoard();
-})
+
+    function resetGame() {
+        minefield = null;
+        minefield = new Minefield(10);
+        minefield.initializeMines();
+        minefield.drawBoard();
+        updateRemainingGuesses(10);
+        updateLives(3);
+    }
+
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetGame);
+
+    function updateRemainingGuesses(guesses) {
+        const remainingGuessesElement = document.querySelector('.left-panel h2:nth-child(2)');
+        remainingGuessesElement.textContent = `Remaining Guesses: ${guesses}`;
+    }
+
+    function updateLives(lives) {
+        const livesElement = document.querySelector('.left-panel h2:nth-child(3)');
+        livesElement.textContent = `Lives: ${lives}`;
+    }
+
+    // Initial display update
+    updateRemainingGuesses(10);
+    updateLives(3);
+});
